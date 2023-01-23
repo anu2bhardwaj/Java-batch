8e1a547dc3083fc84ac1ffbe4ec181e2a5aa85ef
anu2bhardwaj <anu2bhardwaj@gamil.com>
2023-01-23 22:05:24 +0100

    Commit and push the code

---
+++ Projectgitcodeshare/src/pkg1/class2.java
@@ -0,0 +1,13 @@
+package pkg1;
+
+public class class2 {
+	public void method(int a)
+	{
+		System.out.println("i am the main method");
+	}
+public static void main(String[] args) 
+{
+	class2 obj= new class2();
+
+}
+}
