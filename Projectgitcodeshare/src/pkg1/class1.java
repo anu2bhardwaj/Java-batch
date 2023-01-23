2315251c004ed5eea48603d69ec21bf99b14d8bb
anu <anu2bhardwaj@gmail.com>
2023-01-23 21:29:30 +0100

    First commit

---
+++ Projectgitcodeshare/src/pkg1/class1.java
@@ -0,0 +1,40 @@
+package pkg1;
+
+
+public class class1 {
+	
+	 int age;
+	 int roll;
+	 public void display1() 
+	 {
+		 System.out.println("welcome to all of you" );
+	 }
+	 public void display2() 
+	 {
+		 System.out.println("Automation is very easy" );
+	 }
+	 public static void main(String[] args) 
+	 {
+		class1 abc=new class1();
+		abc.age=14;
+	    System.out.println(abc.age );
+	    abc.roll=10;
+	    System.out.println(abc.roll );
+	    abc.display1();
+	    abc.display2();
+				
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
